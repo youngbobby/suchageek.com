b245df90c9eb9ae3e7f9644c96a2151e53c6c1e8
Emmanuel Ibe <theboblife@icloud.com>
2023-07-03 15:32:01 -0500

    Minor changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/content',
+    // '@nuxtjs/tailwindcss',
+    // 'latex.css'
+  ],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://latex.now.sh/style.css'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://latex.now.sh/prism/prism.css'
+        }
+      ]
+    }
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Hi, I'm Emmanuel, this is where I write</h2>
+    </div>
+
+    <div>
+        <h2>Bio</h2>
+        <p>Sebastian is a software developer and tinkerer who has spent over a decade building web applications across
+            healthcare, media, and finance sectors. In 2013 Sebastian founded Datablast - a software development & data
+            consultancy, with clients as diverse as the NHS, Global Radio, Strava, Barclay's and Schroders. Sebastian also
+            spends his time creating side projects and products, a process that he calls "Tinkering with convexity".
+        </p>
+
+        <!-- <p>
+            Sebastian holds a BA and MA from the University of Chester, and an MSc in Software Engineering from the
+            University of Oxford. He is a (very bad) hobby photographer, and lives in London.
+        </p> -->
+    </div>
+
+
+    <div>
+        <h2>Popular posts</h2>
+        <ul>
+            <li><a href="#">Cooking For Founders</a> Jan 10, 2021</li>
+        </ul>
+    </div>
+
+
+    <div class="abstract">
+        <h2>Abstract</h2>
+        <p>...</p>
+    </div>
+</template>
+  
